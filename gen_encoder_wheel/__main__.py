ec043781ba3c9bbe26ac8cd85fcff83b6a182ff1
xloem <0xloem@gmail.com>
2022-02-12 17:46:10 -0500

    added click

--- gen_encoder_wheel/__main__.py
+++ gen_encoder_wheel/__main__.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 from PIL import Image
+import click
+
+@click.command()
+@click.option('--size', default=4096, help='Height of wheel.')
+@click.option('--ratio', default=1.0, help='Aspect ratio of wheel. width = ratio * size')
+@click.option('--dpi', default=1, help='Size multiplier.')
+@click.option('--output', help='Output filename.')
+def generate(size, ratio, dpi, output):
+    height = size * dpi
+    width = height * ratio
 
 if __name__ == '__main__':
-    print('Hello, world!')
+    generate()
